3121b9f6a02a3829452e060d01993f194b0a1e02
lunaticAJ <chhajer.3@iitj.ac.in>
2023-12-09 19:26:57 +0530

    Test runner

---
+++ My project/Assets/Scripts/CharacterSwitcher.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class CharacterSwitcher : MonoBehaviour
+{
+    public GameObject p1;
+    public GameObject p2;
+    public Camera mainCamera;
+    public float switchSpeed = 5f; // Adjust the speed of the camera switch
+
+    private PlayerController p1Controller;
+    private PlayerController p2Controller;
+
+    void Start()
+    {
+        // Ensure that p1, p2, and mainCamera are assigned in the Unity Editor.
+        if (p1 == null || p2 == null || mainCamera == null)
+        {
+            Debug.LogError("Assign the player GameObjects (p1 and p2) and the main camera in the Unity Editor.");
+            return;
+        }
+
+        // Get the PlayerController scripts from the player GameObjects.
+        p1Controller = p1.GetComponent<PlayerController>();
+        p2Controller = p2.GetComponent<PlayerController>();
+
+        // Enable controls for p1 and disable controls for p2 at the start.
+        p1Controller.EnableControls();
+        p2Controller.DisableControls();
+    }
+
+    void Update()
+    {
+        // If the 'K' key is pressed, switch between p1 and p2 controls.
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SwitchCharacter();
+        }
+
+        // Update the camera's position to smoothly follow the selected character's position.
+        Vector3 targetPosition = p1Controller.enabled ? p1.transform.position : p2.transform.position;
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(targetPosition.x, mainCamera.transform.position.y, mainCamera.transform.position.z), switchSpeed * Time.deltaTime);
+    }
+
+    void SwitchCharacter()
+    {
+        if (p1Controller.enabled)
+        {
+            p1Controller.DisableControls();
+            p2Controller.EnableControls();
+        }
+        else
+        {
+            p1Controller.EnableControls();
+            p2Controller.DisableControls();
+        }
+    }
+}
---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float jumpForce = 4f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    // Adjust this factor to control how fast the cube falls (opposite gravity)
+    public float gravityFactor;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        EnableControls(); // Enable controls by default
+    }
+
+    void Update()
+    {
+        // Handle player input for movement
+        if (!enabled) return; // Don't process input if controls are disabled
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+
+        // Handle jumping only when grounded
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce * gravityFactor, ForceMode.Impulse);
+            isGrounded = false; // Set to false when jumping
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Manually apply "upside-down" gravity
+        if(gravityFactor == -1f)
+        {
+            rb.AddForce(2*Vector3.up * Physics.gravity.y * gravityFactor, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(Vector3.up * Physics.gravity.y * gravityFactor, ForceMode.Acceleration);
+        }
+    }
+
+    // Check if the player is grounded
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    // Enable controls and set grounded to true when enabled
+    public void EnableControls()
+    {
+        enabled = true;
+    }
+
+    public void DisableControls()
+    {
+        enabled = false;
+    }
+}
