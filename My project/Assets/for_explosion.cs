271f93985a6bfb403f95601788710709c8ae3575
adityakedia006 <b22ee006@iitj.ac.in>
2023-12-11 20:28:53 +0530

    animation for plane crashing

---
+++ My project/Assets/for_explosion.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class for_explosion : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SetActiveExplosionPlane", 2f);
+        explosionPlane.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Function to set active explosion_plane particle system
+    public void SetActiveExplosionPlane()
+    {
+        if (!explosionPlane.activeSelf)
+        {
+            explosionPlane.SetActive(true);
+        }
+    }
+}
