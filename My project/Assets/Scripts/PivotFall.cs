1ee75df8fc22bfa3bb5f94a56effa729469c0393
lunaticAJ <chhajer.3@iitj.ac.in>
2023-12-12 16:42:50 +0530

    pivot bug

--- My project/Assets/Scripts/PivotFall.cs
+++ My project/Assets/Scripts/PivotFall.cs
@@ -8,14 +8,12 @@ public class PivotFall : MonoBehaviour
     public GameObject p2;
     public GameObject button2;
 
-    private void OnCollisionEnter(Collision collision)
+    private void Update()
     {
-        if (collision.gameObject.name == "DefaultMalePBR")
+        if (p2.transform.position.x >= button2.transform.position.x - 0.2f && p2.transform.position.x <= button2.transform.position.x + 0.2f && p2.transform.position.y + 0.65f >= button2.transform.position.y)
         {
-            button2.transform.position = Vector3.Lerp(button2.transform.position, new Vector3(button2.transform.position.x, button2.transform.position.y - 0.1f, button2.transform.position.z), 1f * Time.deltaTime);
-            _ = new WaitForSeconds(1f);
-            //call function to rotate pivot 22 degrees on z axis smoothly
             pivot.transform.rotation = Quaternion.Lerp(pivot.transform.rotation, Quaternion.Euler(0, 0, 22), 1f * Time.deltaTime);
+            button2.transform.position = Vector3.Lerp(button2.transform.position, new Vector3(button2.transform.position.x, 1.7f - 0.1f, button2.transform.position.z), 1f * Time.deltaTime);
         }
     }
 }
