452e35ce98cbee34223a2d76bf2f34897bd14de3
lunaticAJ <chhajer.3@iitj.ac.in>
2023-12-13 22:54:58 +0530

    tempRunner postFixes

--- My project/Assets/Scripts/BridgePress.cs
+++ My project/Assets/Scripts/BridgePress.cs
@@ -19,10 +19,6 @@ public class BridgePress : MonoBehaviour
         //if p2 is in contact with button,make y pos of bridge1 and bridge2 decrease by 1.325f smoothly
         if (p2.transform.position.x >= button.transform.position.x - 0.2f && p2.transform.position.x <= button.transform.position.x + 0.2f && p2.transform.position.y +0.35f >= button.transform.position.y)
         {
-            //bridge1.transform.position = Vector3.Lerp(bridge1.transform.position, new Vector3(bridge1.transform.position.x, bridge1.transform.position.y - 0.625f, bridge1.transform.position.z), 1f * Time.deltaTime);
-            //bridge2.transform.position = Vector3.Lerp(bridge2.transform.position, new Vector3(bridge2.transform.position.x, bridge2.transform.position.y - 0.625f, bridge2.transform.position.z), 1f * Time.deltaTime);
-            //increase y pos of button by 0.1f
-            //button.transform.position = Vector3.Lerp(button.transform.position, new Vector3(button.transform.position.x, -4.6f + 0.1f, button.transform.position.z), 1f * Time.deltaTime);
 
             done = true;
             StartCoroutine(LerpPosition(new Vector3(button.transform.position.x, button.transform.position.y + 0.1f, button.transform.position.z), 2f));
