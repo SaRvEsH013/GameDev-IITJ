f705fbcd6263efcf19807bada5ff8bc871499f39
lunaticAJ <chhajer.3@iitj.ac.in>
2023-12-15 02:39:20 +0530

    tpRunner button fix

--- My project/Assets/Scripts/PivotFall.cs
+++ My project/Assets/Scripts/PivotFall.cs
@@ -11,7 +11,7 @@ public class PivotFall : MonoBehaviour
 
     private void Update()
     {
-        if (p2.transform.position.x >= button2.transform.position.x - 0.2f && p2.transform.position.x <= button2.transform.position.x + 0.2f && p2.transform.position.y + 0.65f >= button2.transform.position.y)
+        if (p2.transform.position.x >= button2.transform.position.x - 0.2f && p2.transform.position.x <= button2.transform.position.x + 0.2f && p2.transform.position.y  <= button2.transform.position.y + 0.65f)
         {
             /*pivot.transform.rotation = Quaternion.Lerp(pivot.transform.rotation, Quaternion.Euler(0, 0, 22), 1f * Time.deltaTime);
             button2.transform.position = Vector3.Lerp(button2.transform.position, new Vector3(button2.transform.position.x, 1.7f - 0.1f, button2.transform.position.z), 1f * Time.deltaTime);*/
