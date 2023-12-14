f74b2566feb57b0d85ac8ca927c7e213de992ce0
Samaksh Verma <vsamaksH@gmail.com>
2023-12-15 01:01:10 +0530

    pushing volcano script

---
+++ My project/Assets/volcano_script.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class volcano_script : MonoBehaviour
+{
+    public PlayableDirector director;
+    private double time; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = director.time;
+        if (time > 2.1)
+        {
+            print("ook");
+            SceneManager.LoadScene("Cube Jump");
+        }
+    }
+}
